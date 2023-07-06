5d81eca2bf912f6455d0eb2d4eb25ed2de5a5bdc
Jose David <Jd>
2023-07-06 12:09:19 +0200

    alarma funciona

--- codigo.py
+++ codigo.py
@@ -1,10 +1,32 @@
 import time
+import pygame
+import threading
+import mysql.connector
 
-from playsound import playsound
+
+
+import mysql.connector
+
+sql_conect = mysql.connector.connect(
+    host='localhost',
+    port=3306,
+    user='root',
+    password='OjoCuidao',
+    database='alarma'
+)
+cur = sql_conect.cursor()
+cur.execute("SELECT id, hora FROM alarma")
+
+for id, hora in cur.fetchall():
+    print(id, hora)
+
+cur.close()
+sql_conect.close()
 
 
 class Alarma:
-    def init(self, hora_alarma):
+
+    def __init__(self, hora_alarma):
         self.hora_alarma = hora_alarma
 
     def validar_hora(self):
@@ -16,47 +38,64 @@ class Alarma:
 
     def mostrar_mensaje(self):
         print("¡DESPIERTA!")
-        playsound("C:\Users\Manal\Downloads\gun.mp3")
+        pygame.mixer.init()
+        pygame.mixer.music.load("sound/buenosdias.mp3")
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)
 
     def posponer_alarma(self):
         print("La alarma se pospone 5 minutos.")
-        time.sleep(3)
-        playsound("C:\Users\Manal\Downloads\gun.mp3")
+        time.sleep(300)  
+        self.mostrar_mensaje()
 
     def detener_alarma(self):
         print("La alarma se ha detenido.")
 
     def iniciar_alarma(self):
         while True:
-            if self.validar_hora():
-                break
-            else:
-                print("Hora invalida, intentalo otra vez.")
+            while not self.validar_hora():
+                print("Hora inválida, inténtalo otra vez.")
+                hora_alarma = input("Ingrese la hora de la alarma (formato HH:MM): ")
+                self.hora_alarma = hora_alarma
 
-        hora_alarma = time.strptime(self.hora_alarma, "%H:%M")
+            hora_alarma = time.strptime(self.hora_alarma, "%H:%M")
 
-        while True:
-            hora_actual = time.localtime()
+            while True:
+                hora_actual = time.localtime()
 
-            if (
-                hora_actual.tm_hour == hora_alarma.tm_hour
-                and hora_actual.tm_min == hora_alarma.tm_min
-            ):
-                self.mostrar_mensaje()
-                while True:
-                    opcion = input("¿Deseas posponer la alarma? (s/n): ")
-                    if opcion.lower() == "s":
-                        self.posponer_alarma()
-                        break
-                    elif opcion.lower() == "n":
-                        self.detener_alarma()
-                        return
-                    else:
-                        print("Opción inválida. Intente nuevamente.")
+                if hora_actual.tm_hour == hora_alarma.tm_hour and hora_actual.tm_min == hora_alarma.tm_min:
+                    self.mostrar_mensaje()
 
-            time.sleep(1)
+                    while True:
+                        opcion = input("¿Deseas posponer la alarma? (s/n): ")
+                        if opcion.lower() == "s":
+                            self.posponer_alarma()
+                            break
+                        elif opcion.lower() == "n":
+                            self.detener_alarma()
+                            return
+                        else:
+                            print("Opción inválida. Intente nuevamente.")
+
+                time.sleep(1)
+
+                
+                
+
+def configurar_alarma():
+    hora_alarma = input("Ingrese la hora de la alarma (formato HH:MM): ")
+    alarma = Alarma(hora_alarma)
+    alarma.iniciar_alarma()
 
 
-hora_alarma = input("Ingrese la hora de la alarma (formato HH:MM): ")
-alarma = Alarma(hora_alarma)
-alarma.iniciar_alarma()
+num_alarmas = int(input("Ingrese el número de alarmas que desea configurar: "))
+
+threads = []
+for _ in range(num_alarmas):
+    t = threading.Thread(target=configurar_alarma)
+    t.start()
+    threads.append(t)
+
+for thread in threads:
+    thread.join()
